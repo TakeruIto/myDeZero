4f5d57860275affe66130cc8ec5f57caaa38c503
伊藤 武尊 <takeru.ito110632@e-seikatsu.co.jp>
2020-09-12 22:37:03 +0900

    step40 ブロードキャストを行う関数を追加

---
+++ steps/step40.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+x0 = Variable(np.array([1, 2, 3  ]))
+x1 = Variable(np.array([10]))
+y = x0 + x1
+print(y)
+y.backward(create_graph=True)
+print(x1.grad)
