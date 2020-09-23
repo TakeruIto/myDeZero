c1aa9f2c02f6607d9b55c86f2be6e917597906f1
伊藤 武尊 <takeru.ito110632@e-seikatsu.co.jp>
2020-09-23 11:02:01 +0900

    step54 dropoutとテストモードを追加

---
+++ steps/step54.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append('..')
+import numpy as np
+from dezero import test_mode
+import dezero.functions as F
+
+x = np.ones(5)
+print(x)
+
+y = F.dropout(x)
+print(y)
+
+with test_mode():
+    y = F.dropout(x)
+    print(y)
